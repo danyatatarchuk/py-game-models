808cc61d0b537b2f21d93e094f75561ee93cb155
Danil Tatarchuk <tatarchuk.danil06@gmail.com>
2025-09-16 19:36:18 +0300

    fix newline and character

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ def main() -> None:
     for player, player_data in players.items():
         race_data = player_data["race"]
         race, created = Race.objects.get_or_create(
-            name=race_data["name"], description=race_data["description"]
+            name=race_data["name"],
+            defaults={"description": race_data.get("description")}
         )
         for skill_data in race_data["skills"]:
             Skill.objects.get_or_create(
                 name=skill_data["name"], bonus=skill_data["bonus"],
                 race=race
             )
-        guild_data = player_data["guild"]
+        guild_data = player_data.get("guild")
         guild = None
         if guild_data:
             guild, created = Guild.objects.get_or_create(
